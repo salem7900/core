095c53979658c1c9692fce4c0e6f028df2a15530
Salem Abdallah <sabdalla@c2r3s5.42roma.it>
2026-06-08 14:37:06 +0200

    getnextline changes01

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,18 +6,33 @@
 /*   By: sabdalla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 13:39:29 by sabdalla          #+#    #+#             */
-/*   Updated: 2026/06/03 13:44:25 by sabdalla         ###   ########.fr       */
+/*   Updated: 2026/06/08 13:41:22 by sabdalla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static char *newline(int fd, char *ptr)
+{
+	size_t	n;
+	char	*buffer;
+	size_t	nbyte;
+
+	n = 0;
+	buffer = malloc(BUFFER_SIZE + 1);
+	nbyte = read(fd, buffer, BUFFER_SIZE);
+	if(nbyte > 0)
+		buffer[nbyte] = '\0';
+	while (buffer[n])
+	{
+		*ptr = buffer[n];
+		if (buffer[n] == '\n')
+			return (ptr);
 char	*get_next_line(int fd)
 {
-    void           *ptr;
-    size_t          buffer;
-    static size_t   n;
-
-    n = read(fd, &ptr[buffer], buffer);
-    return (ptr);
+	static char	*ptr;
+	size_t          buffer;
+	size_t		n;
+	
+	return (ptr);
 }
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -12,3 +12,149 @@
 
 #include "get_next_line.h"
 
+size_t	ft_strlen(const char *s)
+{
+	size_t	n;
+
+	n = 0;
+	while (s[n])
+	{
+		n++;
+	}
+	return (n);
+}
+
+char	*ft_strdup(const char *s)
+{
+	int		len;
+	char	*dest;
+	int		i;
+
+	len = 0;
+	while (s[len])
+		len++;
+	dest = malloc(sizeof(char) * len + 1);
+	if (dest == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		dest[i] = s[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+static char	*destmal(char const *s1, char const *s2)
+{
+	size_t	len;
+	size_t	len2;
+	char	*dest;
+
+	len = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	dest = malloc(sizeof(char) * (len + len2 + 1));
+	if (dest == NULL)
+		return (NULL);
+	return (dest);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dest;
+	size_t	i;
+	size_t	n;
+
+	if (!s1 || !s2)
+		return (NULL);
+	dest = destmal(s1, s2);
+	if (dest == NULL)
+		return (NULL);
+	i = 0;
+	n = 0;
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	while (s2[n])
+	{
+		dest[i] = s2[n];
+		i++;
+		n++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	const char	*p;
+	size_t		i;
+
+	i = 0;
+	p = s;
+	while (p[i])
+	{
+		if (p[i] == (char)c)
+			return ((char *)p + i);
+		i++;
+	}
+	if (p[i] == (char)c)
+		return ((char *)p + i);
+	return (NULL);
+}
+
+static char	*destall(char const *s, unsigned int start, size_t len)
+{
+	size_t	s_len;
+	char	*dest;
+
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+	{
+		dest = malloc(sizeof(char) * 1);
+		if (!dest)
+			return (NULL);
+		dest[0] = '\0';
+		return (dest);
+	}
+	dest = malloc(sizeof(char) * (len + 1));
+	if (dest == NULL)
+		return (NULL);
+	return (dest);
+}
+
+static size_t	lencheck(char const *s, unsigned int start, size_t len)
+{
+	size_t	s_len;
+
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (0);
+	if (len > s_len - start)
+		len = s_len - start;
+	return (len);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*dest;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	len = lencheck(s, start, len);
+	dest = destall(s, start, len);
+	if (dest == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		dest[i] = s[start + i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
